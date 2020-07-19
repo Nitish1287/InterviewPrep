d562a87356149a0fbaa78d24ed81c424a0c064a2
karthik <karthikrt335@gmail.com@users.noreply.github.com>
2020-07-18 19:18:42 -0700

    Update 01knapsack.java

--- InterviewPrep/Algos/Dynamic Programming/0:1 Knapsack/01Knapsack/01knapsack.java
+++ InterviewPrep/Algos/Dynamic Programming/0:1 Knapsack/01Knapsack/01knapsack.java
@@ -1,6 +1,6 @@
 Recursive - Brute Force Approach:
 check combination of all weights
-starting for last item, for each item check two cases a)with item b)without item
+starting from last item, for each item check two cases a)with item b)without item
 public int solveKnapsack(int[] profits, int[] weights, int capacity) {
 	int maxProfit = 0;
 	int n = weights.length;
@@ -16,8 +16,7 @@ public int helper(int[] profits, int[] weights, int currCapacity, int currWeight
 	if(weights[currWeightIndex]>currCapacity){
 		return helper(profits, weights, currCapacity, currWeightIndex-1);
 	}else{
-		return Math.max(
-										helper(profits, weights, currCapacity, currWeightIndex-1), 
+		return Math.max( helper(profits, weights, currCapacity, currWeightIndex-1), 
 			profits[currWeightIndex]+helper(profits, weights, currCapacity-weights[currWeightIndex], currWeightIndex-1)
 			);
 	}
@@ -30,7 +29,7 @@ Time complexity - O(2^n).. each item branches into two paths
 Space complexity - O(n).. recursion stack of depth n
 ==========================================================================================================================
 
-Recursive - DP:
+Recursive - DP: Top down
 public int solveKnapsack(int[] profits, int[] weights, int capacity) {
 	int maxProfit = 0;
 	int n = weights.length;
@@ -61,6 +60,28 @@ public int helper(int[] profits, int[] weights, int currCapacity, int currWeight
 
 
 Time complexity - O(n * capacity).. once an index and capicity combination is calculated, it can be used by memorization
-Space complexity - O(n).. recursion stack of depth n
+Space complexity - ??
 
-===========================================================================================================================
+============================================================================================================================
+
+Iterative - DP: Bottom up
+public int solveKnapsack(int[] profits, int[] weights, int capacity) {
+	int n = weights.length;
+	int[][] dp = new int[n][capacity+1];
+
+	for(int i=0; i<n; i++){
+		for(int j=1; j<=capacity; j++){
+			if(j>=weights[i]){
+				dp[i][j] = Math.max(dp[i-1][j], profits[i]+dp[i-1][j-weights[i]]);
+			}else{
+				dp[i][j] = dp[i-1][j];
+			}
+		}
+	}
+
+    return dp[n][capacity];
+}
+
+
+Time complexity - O(n * capacity)
+Space complexity - O(n * capacity)
